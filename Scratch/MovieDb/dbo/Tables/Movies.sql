beff543239ceff4a33f1afcf93deb6b68ddc3104
Huynv <huytb2011@gmail.com>
2022-12-19 19:08:17 +0700

    update

--- Scratch/MovieDb/dbo/Tables/Movies.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Movies] (
-    [Id]          INT             IDENTITY (1, 1) NOT NULL,
-    [Title]       NVARCHAR (MAX)  NULL,
-    [ReleaseDate] DATETIME2 (7)   NOT NULL,
-    [Genre]       NVARCHAR (MAX)  NULL,
-    [Price]       DECIMAL (18, 2) NOT NULL,
-    CONSTRAINT [PK_Movies] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
