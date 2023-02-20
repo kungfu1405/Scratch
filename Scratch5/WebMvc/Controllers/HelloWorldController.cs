38cd1bb068b1c0c682de0fd66ca91d1d9f79f8e1
huy nv <huytb2011@gmail.com>
2023-02-20 16:56:34 +0700

    feat:first commit

---
+++ Scratch5/WebMvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
